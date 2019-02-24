d5ddfcc4b45819f95c5f53fbc2809aaeda28231c
Paul Bruce <me@paulsbruce.io>
2019-02-24 09:17:50 -0500

    Added cAdvisor collector. Added API calls to demo-mixed.

---
+++ team/vus/c@advisor/%resources%/scripts/jsAction_2a14aaca-aff3-464b-ab5c-8359935d5186.js
@@ -0,0 +1,12 @@
+﻿// Javascript skeleton.
+// Edit and adapt to your needs.
+// The documentation of the NeoLoad Javascript API
+// is available in the appendix of the documentation.
+
+// Get variable value from VariableManager
+var iterator = context.variableManager.getValue("loopContainers_counter");
+var containerPath = context.variableManager.getValue("containerNames_"+iterator);
+var parts = containerPath.split('/')
+var containerId = parts[parts.length-1]
+context.variableManager.setValue("containerPath",containerPath);
+context.variableManager.setValue("containerId",containerId);
---
+++ team/vus/c@advisor/%resources%/scripts/jsAction_5829c81a-6920-461b-8abf-6b750ea1d944.js
@@ -0,0 +1,32 @@
+﻿// Javascript skeleton.
+// Edit and adapt to your needs.
+// The documentation of the NeoLoad Javascript API
+// is available in the appendix of the documentation.
+
+// Get variable value from VariableManager
+var statPath = context.variableManager.getValue("cAdvisorStats.pathFromStatsNode");
+var json = context.variableManager.getValue("topStatJSON");
+var containerPath = context.variableManager.getValue("containerPath");
+var containerId = context.variableManager.getValue("containerId");
+var containerName = context.variableManager.getValue("containerName");
+
+var value = com.jayway.jsonpath.JsonPath.parse(json).read("$."+statPath,java.lang.Double);
+
+var isValid = (value != null && value != undefined && !isNaN(parseFloat(value)))
+if(!isValid)
+    logger.error("cAdvisor ["+statPath+"] isn't a double: ["+value+"] in...\n"+json)
+
+var extPath = ""
+var cParts = containerPath.split('/')
+for(var i=0; i<cParts.length; i++) {
+    var cPart = cParts[i]
+    if(cPart.trim().length > 0)
+        extPath += (extPath.length > 0 ? "|" : "") + (cPart == containerId ? containerName : cPart)
+}
+var statPathA = statPath;
+while(statPathA.indexOf(".")>-1) { statPathA = statPathA.replace(".","|") }
+extPath += "|"+statPathA
+
+// Inject the computed value in a runtime variable
+context.variableManager.setValue("statPath",extPath);
+context.variableManager.setValue("statValue",value);
